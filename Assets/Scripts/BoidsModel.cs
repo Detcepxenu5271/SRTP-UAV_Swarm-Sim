7421698da7f00634231ab0f95c75a170c7d9a337
Detcepxenu5271 <2296114320@qq.com>
2022-11-07 23:06:41 +0800

    beta 0.1 - Framework of AgentManager and Agent
    * Basic framework of AgentManager and Agent
    * Implement a simple BoidsModel for test

---
+++ Assets/Scripts/BoidsModel.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsModel : MonoBehaviour
+{
+    public static BoidsModel instance; // 单例模式
+
+    // 乘数因子
+    public float total_factor;
+    public float cohension_factor;
+    public float seperation_factor;
+    public float alignment_factor;
+    public float target_factor;
+
+    public bool is_use_target;
+    public Vector3 target_position;
+
+    // 【？】使用相同的邻居，半径、视野等定义在 Agent 中
+    // 三种力的生效半径
+    // public float cohension_radius;
+    // public float seperate_radius;
+    // public float alignment_radius;
+
+    // private Agent agent; // 要计算的智能体
+    // private List<Agent> neighbour_list; // agent 的邻居（依据附近一定范围内其他 agent 的位置进行决策）
+
+    /** 凝聚力 */
+    private Vector3 Cohension(Agent agent, List<Agent> neighbour_list) {
+        Vector3 dir = Vector3.zero; // 用于加总各邻居的位置，计算中心点，再计算凝聚方向
+
+        // 没有邻居，直接返回
+        if (neighbour_list.Count == 0) {
+            return dir;
+        }
+
+        // 计算邻居中心点
+        foreach (Agent neighbour in neighbour_list) {
+            dir += neighbour.Position;
+        }
+        dir /= neighbour_list.Count;
+
+        // 计算 agent 指向 neighbour 中心的的向量
+        dir -= agent.Position;
+
+        return dir.normalized; // 返回单位方向向量
+    }
+    /** 分离力 */
+    private Vector3 Seperation(Agent agent, List<Agent> neighbour_list) {
+        Vector3 dir = Vector3.zero; // 用于加总各邻居的分离向量，得出分离方向
+
+        // 没有邻居，直接返回
+        if (neighbour_list.Count == 0) {
+            return dir;
+        }
+
+        // 累加分离向量
+        foreach (Agent neighbour in neighbour_list) {
+            Vector3 reverse_dir = agent.Position - neighbour.Position;
+            dir += reverse_dir.normalized / reverse_dir.magnitude; // 距离越近，分离向量越大
+        }
+
+        return dir.normalized; // 返回单位方向向量
+    }
+    /** 对齐力 */
+    private Vector3 Alignment(Agent agent, List<Agent> neighbour_list) {
+        Vector3 dir = Vector3.zero; // 用于加总各邻居的速度向量，得出对齐方向
+
+        // 没有邻居，直接返回
+        if (neighbour_list.Count == 0) {
+            return dir;
+        }
+
+        // 累加速度方向
+        foreach (Agent neighbour in neighbour_list) {
+            dir += neighbour.Velocity;
+        }
+
+        return dir.normalized; // 返回单位方向向量
+    }
+    /** 目标力 */
+    private Vector3 TargetForce(Agent agent, Vector3 target_position) {
+        Vector3 dir = Vector3.zero;
+        dir = target_position - agent.Position;
+        return dir.normalized; // 返回单位方向向量
+    }
+    /** 将各种力结合到一起，计算最终加速度 */
+    public Vector3 CalcAccelerate(Agent agent, List<Agent> neighbour_list) {
+        Vector3 dir = Vector3.zero;
+        dir += Cohension(agent, neighbour_list) * cohension_factor;
+        dir += Seperation(agent, neighbour_list) * seperation_factor;
+        dir += Alignment(agent, neighbour_list) * alignment_factor;
+        if (is_use_target) {
+            dir += TargetForce(agent, target_position) * target_factor;
+        }
+        return dir;
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+}
---
+++ Assets/Scripts/MyMath.cs
@@ -0,0 +1,33 @@
+using System; // Mathf
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** 数学相关函数
+ * 描述：将一些项目中常用的数学方法集中到 MyMath 单例类中
+ * 使用方法：MyMath.instance.要调用的函数() */
+public class MyMath : MonoBehaviour
+{
+    public static MyMath instance; // 单例模式
+
+    public const float PRECISION = 0.000001f; // 1e-6
+
+    private void Awake() {
+        instance = this;
+    }
+
+    /** 将用两个角度表示的速度转化为单位向量
+     * phi：方向和 y 轴的夹角
+     * theta：方向在 xz 平面的投影和 x 轴的夹角 */
+    public Vector3 VelocityAngleToVector(float phi, float theta) {
+        float SinPhi = Mathf.Sin(phi);
+        float CosPhi = Mathf.Cos(phi);
+        float SinTheta = Mathf.Sin(theta);
+        float CosTheta = Mathf.Cos(theta);
+        return new Vector3(SinPhi*CosTheta, CosPhi, SinPhi*SinTheta);
+    }
+
+    public float DegreeToRadian(float degree) {
+        return degree * Mathf.PI / 180f;
+    }
+}
